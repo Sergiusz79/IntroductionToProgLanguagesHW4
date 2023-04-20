8c30953866a080369d4f4b6e601a16d9428ddb5e
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-20 12:27:46 +0200

    Task027

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -6,5 +6,21 @@
 // 82   ->  10
 // 9012 ->  12
 
+int a;
 
+Console.WriteLine("Please enter number: ");
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+    Console.WriteLine("It's not a number! ");
+    }
 
+int sum=0;
+int lgth = (int) Math.Log10(a)+1;
+
+for (int count = 0; lgth > count; count++)
+{
+    int a1 = a / (int) Math.Pow(10, count);
+    sum += a1 % 10;
+}
+
+    Console.WriteLine($"The sum of the digits in a number {sum} !");
--- Task029/Program.cs
+++ Task029/Program.cs
@@ -5,3 +5,28 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
+// int x = int.Parse(Console.ReadLine());
+// int lgth = (int) Math.Log10(x);
+// int sum=0;
+// for (int i=lgth; i>=0; i--)
+// {
+//     sum+=x/(int)Math.Pow(10.0,i);
+//     x=x%(int)Math.Pow(10.0,i);
+
+//     Console.WriteLine($" sum = {sum}");
+
+//     Console.WriteLine($" x = {x}");
+// }
+// Console.WriteLine(sum);
+
+Console.WriteLine("Please enter number: ");
+int lgth = 0;
+    do 
+    {
+        int x = int.Parse(Console.ReadLine());
+        lgth = (int) Math.Log10(x);
+        if (lgth != 4) {Console.WriteLine("It's not five-digits number! ");}
+        
+    }
+    while (lgth != 4);
+Console.WriteLine("It's Super! ");
