3d7f9d117a8c39c3a067a08a5083c125d9d4dbad
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-20 10:20:13 +0200

    AdditionalTask

---
+++ AdditionalTask/Program.cs
@@ -0,0 +1,40 @@
+﻿// Программа принимает на вход длины сторон прямоугольника (A B) и квадрата (С)
+//Узнать сколько данных квадратов поместится внутри даного прямоугольника
+
+
+int a, b, c;
+Console.WriteLine("Please enter the lengths of sides A and B of the rectangle: ");
+CheckReckt();
+
+Console.WriteLine("Please enter the length of the C side of the square: ");
+CheckSquare();
+
+int l = a / c; 
+int w = b / c;
+int am = l * w;
+
+Console.WriteLine($" {am} squares will fit in a given rectangle!");
+
+
+
+
+
+void CheckReckt()
+{
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+    Console.WriteLine("It's not a number! ");
+    }
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+    Console.WriteLine("It's not a number! ");
+    }
+}
+
+void CheckSquare()
+{
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+    Console.WriteLine("It's not a number! ");
+    }
+}
