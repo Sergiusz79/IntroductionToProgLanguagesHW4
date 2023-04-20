0aca370e6abaaff1b9a15d8d1065014a33f01cc4
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-20 10:41:17 +0200

    Task025

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -1,8 +1,27 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
 //и возводит число A в натуральную степень B.
 // Например:
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33       -> [6, 1, 33]
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int a, b;
+for (;;)
+{
+    Console.WriteLine("Please enter number: ");
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+    Console.WriteLine("It's not a number! ");
+    }
+
+    Console.WriteLine("Please enter degree number: ");    
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+    Console.WriteLine("It's not a number! ");
+    }
+    double pow = Math.Pow(a , b);
+
+Console.WriteLine($" Number {a} to the power of {b} = {pow} ! ");
+}
 
 
 
